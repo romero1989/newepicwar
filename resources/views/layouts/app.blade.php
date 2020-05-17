3155e14f36e0cc727bbfe924609625476aef2bfb
Romero Gomes <romero.ufrr@gmail.com>
2020-05-17 19:33:10 -0400

    a

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,15 +10,15 @@
     <title>{{ config('app.name', 'New Epic War') }}</title>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="public/js/app.js" defer></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/site/cover.css') }}" rel="stylesheet">
+    <link href="public/css/app.css" rel="stylesheet">
+    <link href="public/css/site/cover.css" rel="stylesheet">
 </head>
 <body>
     <div id="app">
