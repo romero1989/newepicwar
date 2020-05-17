9ce54224f141b8658bcb3b65c78e27760724f8c1
Romero Gomes <romero.ufrr@gmail.com>
2020-05-17 18:11:21 -0400

    ajuste no arquivo de rotas

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,22 @@ Route::get('/', function () {
     return view('site/index');
 });
 
-Auth::routes();
+//Auth::routes();
+
+// Authentication Routes...
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login');
+Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+
+// Registration Routes...
+Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+Route::post('register', 'Auth\RegisterController@register');
+
+// Password Reset Routes...
+Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm');
+Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail');
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm');
+Route::post('password/reset', 'Auth\ResetPasswordController@reset');
 
 Route::get('app/home', 'HomeController@index')->name('app.home');
 Route::get('app/guild', 'HomeController@guild')->name('app.guildmark');
